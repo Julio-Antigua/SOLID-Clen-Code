8f1a8e6134069e9a01651e2088fe130740cd1b4f
Julio Saul <juliosaul12@gmail.com>
2023-08-28 10:47:55 -0400

    inicio del proyecto

---
+++ src/clean-code/02-name-types.ts
@@ -0,0 +1,47 @@
+(() => {
+
+    const temperaturesCelsius = [33.6, 12.34];
+
+    const serverIpAdress = '123.123.123.123';
+
+    const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    const userEmails = users.map( user => user.email );
+
+    // Variables booleanas de un video juego
+    const isJump = false;
+    const canRun = true;
+    const hasItems = true;
+    const isLoading = false;
+
+
+    const startTime = new Date().getTime();
+    //....
+    // 3 doritos después
+    //...
+    const endTime = new Date().getTime() - startTime;
+
+
+    // Funciones
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    function getBooksByUrl( url: string) {
+        throw new Error('Function not implemented.');
+    }
+    
+    function getASquareArea( sides: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
+    
+    
+
+
+
+
+})();
