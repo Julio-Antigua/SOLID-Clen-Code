d0d51919d5ccdc4cb3ce894bc6d639b37cd5fb26
Julio Saul <juliosaul12@gmail.com>
2023-08-28 16:25:11 -0400

    Se creo el archivo 03-functions

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,35 @@
+(() => {
+
+    function getMovieById( Id: string ) {
+        console.log({ Id });
+    }
+
+    function getMovieCastById( MovieId: string ) {
+        console.log({ MovieId });
+    }
+
+    function getActorBioById( Id: string ) {
+        console.log({ Id });
+    }
+    
+
+    interface Movie{
+        cast:        string[];
+        description: string;
+        rating:      number;
+        title:       string;
+    }
+    function createMovie({title, description, rating, cast }: Movie) {
+        console.log({ title, description, rating, cast });
+    }
+
+    function createActor( fullName: string, birthdate: Date ): boolean {
+
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor', birthdate);
+        return true;        
+
+    }
+
+})();
